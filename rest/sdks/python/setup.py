a95e112cc2849dd9c800598a675a636c1eaa354d
weixuefeng <weixuefeng@lubangame.com>
2019-06-22 18:58:53 +0800

    update setup

--- rest/sdks/python/setup.py
+++ rest/sdks/python/setup.py
@@ -21,7 +21,7 @@ VERSION = "1.0.0"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "fastecdsa == 1.7.3", "pysha3 == 1.0.2"]
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "fastecdsa == 1.7.3", "pysha3 == 1.0.2", "base58 == 1.0.3"]
 
 setup(
     name=NAME,
