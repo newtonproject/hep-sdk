6bacadcc48067000b3d93567ebe66320edfc33cf
pschy <cj82@qq.com>
2019-06-26 19:19:33 +0800

    proofHelper

--- rest/sdks/php/HepRestApi/lib/Scenarios/BaseHelper.php
+++ rest/sdks/php/HepRestApi/lib/Scenarios/BaseHelper.php
@@ -3,8 +3,154 @@
 
 namespace HepRestApi\Scenarios;
 
+use HepRestApi\Utils;
+use HepRestApi\ApiException;
+use HepRestApi\Api\RestApi;
+use Ramsey\Uuid\Uuid;
 
 class BaseHelper
 {
+    /* @var RestApi $api_client */
+    protected $api_client;
 
+    protected $base_parameters, $dapp_id, $dapp_secret, $private_key, $api_version, $chain_id;
+
+    public function __construct(RestApi $api_client, $base_parameters, $dapp_id, $dapp_secret, $private_key, $api_version='1', $chain_id=1012)
+    {
+        if (!isset($base_parameters['dapp_key']) || empty($base_parameters['dapp_key'])) {
+            throw new ApiException("Missing the required parameter `dapp_key` when calling `BaseHelper`");
+        }
+        # verify the required parameter 'protocol' is set
+        if (!isset($base_parameters['protocol']) || empty($base_parameters['protocol'])) {
+            throw new ApiException("Missing the required parameter `protocol` when calling `BaseHelper`");
+        }
+        # verify the required parameter 'version' is set
+        if (!isset($base_parameters['version']) || empty($base_parameters['version'])) {
+            throw new ApiException("Missing the required parameter `version` when calling `BaseHelper`");
+        }# noqa: E501
+        if (!isset($base_parameters['os']) || empty($base_parameters['os'])) {
+            throw new ApiException("Missing the required parameter `os` when calling `BaseHelper`");
+        }  # noqa: E501
+        # verify the required parameter 'language' is set
+        if (!isset($base_parameters['language']) || empty($base_parameters['language'])) {
+            throw new ApiException("Missing the required parameter `language` when calling `BaseHelper`");
+        }
+
+        $this->api_client = $api_client;
+        $this->base_parameters = $base_parameters;
+        $this->private_key = $private_key;
+        $this->api_version = $api_version;
+        $this->dapp_secret = $dapp_secret;
+        $this->action_auth_login = 'hep.auth.login';
+        $this->action_auth_pay = "hep.pay.order";
+        $this->action_auth_proof = "hep.proof.submit";
+        $this->chain_id = $chain_id;
+        $this->dapp_id = $dapp_id;
+    }
+
+    /**
+     * Retrieve the public key of default trust oracle from hep node
+     * @return array The public key of default trust oracle
+     * @throws ApiException
+     */
+    public function get_default_trust_oracle()
+    {
+        $params = [];
+        $sign_data = $this->generate_sign_data($params);
+        $hmac_data = $this->sign_hmac($sign_data);
+
+        $hmac_data['oracle_id'] = 'default';
+
+        $res = $this->api_client->restOraclesRead(
+            $this->api_version,
+            $hmac_data['oracle_id'],
+            $this->base_parameters['dapp_key'],
+            $this->base_parameters['protocol'],
+            $this->base_parameters['version'],
+            $hmac_data['ts'],
+            $hmac_data['nonce'],
+            $this->base_parameters['os'],
+            $this->base_parameters['language'],
+            $hmac_data['dapp_signature_method'],
+            $hmac_data['dapp_signature']
+        );
+
+        return $res['public_key'];
+    }
+
+    protected function generate_sign_data(array $data = [])
+    {
+        $sign_data = array_merge($data, $this->base_parameters);
+        # dynamic fields
+        $sign_data['ts'] = time();
+        $sign_data['nonce'] = Uuid::uuid4()->toString();
+        return $sign_data;
+    }
+
+    public function sign_hmac(array $data, $dapp_signature_method='HMAC-MD5')
+    {
+        $dapp_signature = Utils::sign_hmac($data, $this->dapp_secret, '', false, '&', $dapp_signature_method);
+
+        $data['dapp_signature'] = $dapp_signature;
+        $data['dapp_signature_method'] = $dapp_signature_method;
+
+        return $data;
+    }
+
+    protected function concat_signature($r, $s)
+    {
+        if (strpos($r, '0x') === 0) {
+            $r = substr($r, 2);
+        }
+        if (strlen($r) < 64) {
+            $r = str_pad($r, 64, "0", STR_PAD_LEFT);
+        }
+
+        if (strpos($s, '0x') === 0) {
+            $s = substr($s, 2);
+        }
+        if (strlen($s) < 64) {
+            $s = str_pad($s, 64, "0", STR_PAD_LEFT);
+        }
+
+        return "0x{$r}{$s}";
+    }
+
+    public function sign_secp256r1($data)
+    {
+        $sign_string = Utils::generate_signature_base_string($data, '&');
+        $sign = Utils::sign($sign_string, $this->private_key);
+        $r = $sign[0];
+        $s = $sign[1];
+        $signature = $this->concat_signature($r, $s);
+        $data['signature'] = $signature;
+        $data['sign_type'] = 'secp256r1';
+        return $data;
+    }
+
+    public function validate_r1_data($data)
+    {
+        $signed_message = Utils::generate_signature_base_string($data, "&");
+        list($r, $s) = $this->split_signature($data['signature']);
+        $valid_public_keys = $this->get_default_trust_oracle();
+
+        return Utils::verify($signed_message, $r, $s, $valid_public_keys);
+    }
+
+    private function split_signature($signature)
+    {
+        if (empty($signature)) {
+            throw new ApiException("Missing the required parameter 'signature' when calling split_signature");
+        }
+
+        if (strpos($signature, '0x') === 0) {
+            $signature = substr($signature, 2);
+        }
+
+        $sig_half_len = strlen($signature) / 2;
+        $r = '0x' . substr($signature, 0, $sig_half_len);
+        $s = '0x' . substr($signature, $sig_half_len);
+
+        return [$r, $s];
+    }
 }
--- rest/sdks/php/HepRestApi/lib/Utils.php
+++ rest/sdks/php/HepRestApi/lib/Utils.php
@@ -1,10 +1,181 @@
 <?php
 
-
 namespace HepRestApi;
 
+use Elliptic\EC;
+use kornrunner\Keccak;
 
-trait Utils
+class Utils
 {
+    static $SUPPORT_SIGNATURE_METHODS = array('HMAC-MD5');
+    /**
+     * 签名前的相关数据的预处理
+     * @param $data
+     * @return string
+     */
+    static function generate_md5($data)
+    {
+        return md5($data);
+    }
 
+    static function generate_signature_base_string($data, $joint)
+    {
+        $sign_string = '';
+        $sign_fields = array('dapp_signature_method', 'dapp_signature', 'signature', 'sign_type');
+        $n           = 0;
+
+        $data = self::sort_array_by_key($data);
+        foreach ($data as $key => $val) {
+            if (in_array($key, $sign_fields)) {
+                continue;
+            }
+            if ($n != 0) {
+                $sign_string = $sign_string . $joint;
+            }
+            $n = $n + 1;
+            if (is_array($val)) {
+                $sign_string = $sign_string . $key . "=" . json_encode($val);
+            } else {
+                $sign_string = $sign_string . $key . "=" . $val;
+            }
+        }
+        return $sign_string;
+    }
+
+    /**
+     * 加密式处理预备签名的字符串
+     * @param array $data 加签的有规则的数组
+     * @param string $secret 哈希的密钥
+     * @param string $prefix 前缀
+     * @param bool $use_urlencode url处理的逻辑值
+     * @param string $joint 连接符
+     * @param string $signature_method 哈希方法
+     * @return string
+     */
+    static function sign_hmac($data, $secret, $prefix = '', $use_urlencode = False, $joint = '&', $signature_method = 'HMAC-MD5')
+    {
+        if (!in_array($signature_method, self::$SUPPORT_SIGNATURE_METHODS)) {
+            echo "Unsupported signature method";
+            exit();
+        }
+        $sign_string = $prefix;
+        $sign_string = $sign_string . self::generate_signature_base_string($data, $joint);
+        $sign_string = $sign_string . $secret;
+        if ($use_urlencode) {
+            $sign_string = urlencode($sign_string);
+        }
+        $signed_string = self::generate_md5($sign_string);
+        return $signed_string;
+    }
+
+    //生成密钥对
+    static function getkeys()
+    {
+        $ec      = new EC('p256');
+        $key     = $ec->genKeyPair();
+        $privkey = $key->getPrivate();
+        $pubkey  = $key->getPublic();
+        $arr[]   = $privkey->jsonSerialize();
+        $x       = $pubkey->getX()->jsonSerialize();
+        $y       = $pubkey->getY()->jsonSerialize();
+
+        if (strlen($x) < 64) {
+            $prefix = "";
+            for ($i = 0; $i < (64 - strlen($x)); $i++) {
+                $prefix = $prefix . "0";
+            }
+            $x = $prefix . $x;
+        }
+
+        if (strlen($y) < 64) {
+            $prefix = "";
+            for ($i = 0; $i < (64 - strlen($x)); $i++) {
+                $prefix = $prefix . "0";
+            }
+            $y = $prefix . $y;
+        }
+
+        $public_key = "0x" . $x . $y;
+        $arr[]      = $public_key;
+        return $arr;
+    }
+
+    //加签名
+    static function sign($msg, $privkey)
+    {
+        $ec = new EC('p256');
+        if (is_string($msg)) {
+            $msg = Keccak::hash($msg, 256);
+        }
+        $signature = $ec->sign($msg, $privkey);
+        $derSign   = $signature->toDER('hex');
+        $darr[]    = $signature->r->jsonSerialize();
+        $darr[]    = $signature->s->jsonSerialize();
+        $darr[]    = $derSign;
+        $darr[]    = "0x" . $signature->r->jsonSerialize() . $signature->s->jsonSerialize();
+        return $darr;
+    }
+
+    //获取公钥
+    static function getpubkeyformsign($msg, $r, $s)
+    {
+        $sign      = array();
+        $sign['r'] = $r;
+        $sign['s'] = $s;
+        $ec        = new EC('p256');
+        for ($l = 0; $l < 2; $l++) {
+            $sign['recoveryParam'] = $l;
+            $pubkeyobj             = $ec->recoverPubKey($msg, $sign, $l, false);
+            $x                     = $pubkeyobj->getX()->jsonSerialize();
+            $y                     = $pubkeyobj->getY()->jsonSerialize();
+            if (strlen($x) < 64) {
+                $prefix = "";
+                for ($i = 0; $i < (64 - strlen($x)); $i++) {
+                    $prefix = $prefix . "0";
+                }
+                $x = $prefix . $x;
+            }
+            if (strlen($y) < 64) {
+                $prefix = "";
+                for ($i = 0; $i < (64 - strlen($x)); $i++) {
+                    $prefix = $prefix . "0";
+                }
+                $y = $prefix . $y;
+            }
+            $arrs[] = "0x" . $x . $y;
+        }
+        return $arrs;
+    }
+
+    //验证签名
+    static function verify($msg, $r, $s, $public_key)
+    {
+        if (is_string($msg)) {
+            $msg = Keccak::hash($msg, 256);
+        }
+        $pukarr = self::getpubkeyformsign($msg, $r, $s);
+        if (in_array($public_key, $pukarr)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 多维数组按键排序
+     * @param $data
+     * @param string $sort_type
+     * @return mixed
+     */
+    static function sort_array_by_key($data, $sort_type = 'ASC')
+    {
+        $sort_methon = $sort_type == 'ASC' ? 'ksort' : 'krsort';
+        $sort_methon($data);
+        foreach ($data as &$value) {
+            if (is_array($value)) {
+                $value = self::sort_array_by_key($value, $sort_type);
+            }
+        }
+        return $data;
+    }
 }
